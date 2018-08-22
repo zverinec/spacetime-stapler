3c1af341a7ae5bf76c43222a9694abdb3fc023c2
Martin Ukrop <mukrop@mail.muni.cz>
2018-08-22 23:59:16 +0200

    Update passwords

--- code_generator/decrypt.py
+++ code_generator/decrypt.py
@@ -8,9 +8,13 @@ with open(file_encrypted, newline='') as csv1, \
      open(file_passwords, newline='') as csv2, \
      open(file_decrypted, 'w', newline='') as csv3:
     csvencrypted = csv.reader(csv1, delimiter=',')
-    csvpasswords = list(csv.reader(csv2, delimiter=','))
+    list_passwords = list(csv.reader(csv2, delimiter=','))
     csvdecrypted = csv.writer(csv3, delimiter=',')
+    passnum = 0
     for dimension_id, encrypted in csvencrypted:
-        for password_id, password in csvpasswords:
+        for i in [0,1]:
+            password_id = list_passwords[passnum][0]
+            password = list_passwords[passnum][1]
             decrypted = format(int(str(encrypted), 16) ^ int(str(password+password+password+password), 16), '0>16X')
             csvdecrypted.writerow([dimension_id+'-'+password_id, encrypted, password, decrypted])
+            passnum +=1
