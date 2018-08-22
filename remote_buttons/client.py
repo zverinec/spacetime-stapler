73fbb5bd3b00002dff8fb60845e537f5aa2ede26
Jan Mrázek <email@honzamrazek.cz>
2018-08-22 13:10:46 +0200

    Show red light at startup

--- remote_buttons/client.py
+++ remote_buttons/client.py
@@ -3,6 +3,7 @@
 import requests
 import argparse
 from timeit import default_timer as timer
+import time
 
 class Every:
     def __init__(self, period):
@@ -37,26 +38,32 @@ if __name__ == "__main__":
     id = x.json()["id"]
     print("got id: {}".format(id))
 
+    register_timer = Every(2)
+    button_deadtime = Every(5)
+    status_time = Every(0.5)
+
     if args.soft:
         button = lambda: keyboard.is_pressed("space")
         reveal = lambda x: print("Revealed") if x else None
     else:
-        ## ToDo
         btn = gpiozero.Button(26)
-        led = gpiozero.LED(12)
+        led_g = gpiozero.LED(16)
+        led_r = gpiozero.LED(20)
         button = lambda: btn.is_pressed
-        reveal = lambda x: led.on() if x else led.off()
+        reveal = lambda x: led_g.on() if x else led_g.off()
+        fail = lambda x: led_r.on() if x else led_r.off()
 
-    register_timer = Every(2)
-    button_deadtime = Every(5)
-    status_time = Every(0.5)
+        led_r.on()
+        time.sleep(5)
+        led_r.off()
+
     while True:
         if status_time.trigger():
-            x = requests.get(server + "status")
-            status = x.json["revealed"]
+            x = requests.get(server + "/status")
+            status = x.json()["revealed"]
             reveal(status)
         if register_timer.trigger():
             requests.post(server + "/register", data={ "id": id } )
-        if button() and button_deadtime.trigger():
+        if not button() and button_deadtime.trigger():
             print("Button was pressed!")
             requests.post(server + "/press", data={ "id": id } )
